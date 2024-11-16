733075333bc377cab9b9e1031eaea2a6391a4b3a
Andreas <awuehrl@gmail.com>
2024-11-16 02:57:14 +0100

    Landing Page Frontend

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,17 +1,5 @@
 import { styled, alpha } from '@mui/material/styles';
-import {
-    AppBar,
-    Box,
-    Button,
-    Card,
-    CardActions,
-    CardContent,
-    CardMedia,
-    InputBase,
-    Toolbar,
-    Typography
-} from "@mui/material";
-import Grid from "@mui/material/Grid2";
+import {AppBar, Box, Button, InputBase, Toolbar} from "@mui/material";
 import SearchIcon from '@mui/icons-material/Search';
 
 const StyledInputBase = styled(InputBase)(({ theme }) => ({
@@ -59,68 +47,26 @@ const logo = {
     imageUrl: '/pictures/RentopiaLogo64.jpg',
 };
 
-/*const Item = styled(Paper)(({ theme }) => ({
-    backgroundColor: '#fff',
-    ...theme.typography.body2,
-    padding: theme.spacing(2),
-    textAlign: 'center',
-    color: theme.palette.text.secondary,
-    ...theme.applyStyles('dark', {
-        backgroundColor: '#1A2027',
-    }),
-}));*/
-
 function App() {
   return (
-      <div>
-          <Box sx={{ flexGrow: 1 }}>
-              <AppBar position="static">
-                  <Toolbar>
-                      <img src={logo.imageUrl} alt={"Rentopia Logo"}/>
-                      <Search sx={{ flexGrow: 0.5 }} >
-                          <SearchIconWrapper>
-                              <SearchIcon/>
-                          </SearchIconWrapper>
-                          <StyledInputBase
-                              placeholder="Search…"
-                              inputProps={{'aria-label': 'search'}}
-                          />
-                      </Search>
-                      <Box sx={{flexGrow: 1}}></Box>
-                      <Button variant="outlined" color="inherit">Sign in</Button>
-                  </Toolbar>
-              </AppBar>
-          </Box>
-          <Box sx={{ flexGrow: 1}} >
-              <Grid container spacing={{xs: 4}} margin-top={"25px"} justifyContent={'center'} >
-                  {Array.from({ length: 15 }).map((_, index) => (
-                      <Grid key={index}>
-                        <Card sx={{maxWidth: 250 }}>
-                            <CardMedia
-                                component="img"
-                                alt={"ToolNr " + index}
-                                height="125"
-                                image="" />
-                            <CardContent>
-                                <Typography gutterBottom variant="h6" component="div">
-                                    {"ToolNr " + index}
-                                </Typography>
-                                <Typography variant="body2" sx={{ color: 'text.secondary' }}>
-                                    {"This Tool is crazy and nice. Pls Rent it. !Lets go Tool " + index}
-                                </Typography>
-                            </CardContent>
-                            <CardActions>
-                                <Button size={"small"}>Show More</Button>
-                                <Typography variant="body1" sx={{ color: 'text.secondary' }}>
-                                    {index * 3 + " €"}
-                                </Typography>
-                            </CardActions>
-                        </Card>
-                      </Grid>
-                  ))}
-              </Grid>
-          </Box>
-      </div>
+      <Box sx={{ flexGrow: 1 }}>
+          <AppBar position="static">
+              <Toolbar>
+                  <img src={logo.imageUrl} alt={"Rentopia Logo"}/>
+                  <Search sx={{ flexGrow: 0.5 }} >
+                      <SearchIconWrapper>
+                          <SearchIcon/>
+                      </SearchIconWrapper>
+                      <StyledInputBase
+                          placeholder="Search…"
+                          inputProps={{'aria-label': 'search'}}
+                      />
+                  </Search>
+                  <Box sx={{flexGrow: 1}}></Box>
+                  <Button variant="outlined" color="inherit">Sign in</Button>
+              </Toolbar>
+          </AppBar>
+      </Box>
   );
 }
 
--- src/main/java/com/othr/rentopia/controller/HomeController.java
+++ src/main/java/com/othr/rentopia/controller/HomeController.java
@@ -1,12 +1,11 @@
 package com.othr.rentopia.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/home")
+@RequestMapping("/api")
 public class HomeController {
 
     @GetMapping("/home")
