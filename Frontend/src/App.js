25203cc30d2df66cefafa0ffa756d83a71124d50
Florian-Preissner <flo.preissner@gmail.com>
2024-11-19 22:03:52 +0100

    Add router structure and prototype for login component

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,72 +1,19 @@
-import { styled, alpha } from '@mui/material/styles';
-import {AppBar, Box, Button, InputBase, Toolbar} from "@mui/material";
-import SearchIcon from '@mui/icons-material/Search';
 
-const StyledInputBase = styled(InputBase)(({ theme }) => ({
-    color: 'inherit',
-    '& .MuiInputBase-input': {
-        padding: theme.spacing(1, 1, 1, 0),
-        // vertical padding + font size from searchIcon
-        paddingLeft: `calc(1em + ${theme.spacing(4)})`,
-        transition: theme.transitions.create('width'),
-        width: '100%',
-        [theme.breakpoints.up('md')]: {
-            width: '20ch',
-        },
-    },
-    width: '100%',
-}));
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import LoginComponent from "./components/LoginComponent.js";
+import Home from "./components/Home.js";
 
-const SearchIconWrapper = styled('div')(({ theme }) => ({
-    padding: theme.spacing(0, 2),
-    height: '100%',
-    position: 'absolute',
-    pointerEvents: 'none',
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-}));
-
-const Search = styled('div')(({ theme }) => ({
-    position: 'relative',
-    borderRadius: theme.shape.borderRadius,
-    backgroundColor: alpha(theme.palette.common.white, 0.15),
-    '&:hover': {
-        backgroundColor: alpha(theme.palette.common.white, 0.25),
-    },
-    marginRight: theme.spacing(2),
-    marginLeft: 0,
-    width: '100%',
-    [theme.breakpoints.up('sm')]: {
-        marginLeft: theme.spacing(3),
-        width: 'auto',
-    },
-}));
-
-const logo = {
-    imageUrl: '/pictures/RentopiaLogo64.jpg',
-};
 
 function App() {
   return (
-      <Box sx={{ flexGrow: 1 }}>
-          <AppBar position="static">
-              <Toolbar>
-                  <img src={logo.imageUrl} alt={"Rentopia Logo"}/>
-                  <Search sx={{ flexGrow: 0.5 }} >
-                      <SearchIconWrapper>
-                          <SearchIcon/>
-                      </SearchIconWrapper>
-                      <StyledInputBase
-                          placeholder="Search…"
-                          inputProps={{'aria-label': 'search'}}
-                      />
-                  </Search>
-                  <Box sx={{flexGrow: 1}}></Box>
-                  <Button variant="outlined" color="inherit">Sign in</Button>
-              </Toolbar>
-          </AppBar>
-      </Box>
+      <BrowserRouter>
+          <Routes>
+              <Route path="/" >
+                  <Route index element={<Home />} />
+                  <Route path="login" element={<LoginComponent />} />
+              </Route>
+          </Routes>
+      </BrowserRouter>
   );
 }
 
