897309e78ea4ba7e6b82580548a575e62e08d82b
Andreas <awuehrl@gmail.com>
2024-11-24 22:12:33 +0100

    add tool cards to landing page

--- src/main/java/com/othr/rentopia/controller/HomeController.java
+++ src/main/java/com/othr/rentopia/controller/HomeController.java
@@ -1,11 +1,12 @@
 package com.othr.rentopia.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/home")
 public class HomeController {
 
     @GetMapping("/home")
