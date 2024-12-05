4065802d202f238fae1c24684de0e3ffa00ab8ec
Jakob Pistohl <gh@jp-bits.de>
2024-12-05 19:58:33 +0100

    Add routes for devices

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,4 +1,4 @@
-import {BrowserRouter, Routes, Route} from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LoginSite from "./sites/Login.js";
 import HomeSite from "./sites/Home.js";
 import ForgetPasswordSite from "./sites/ForgetPassword.js";
@@ -7,22 +7,26 @@ import RegisterSite from "./sites/Register.js";
 import { useEffect } from "react"
 
 function App() {
-/*    useEffect(() => {
-        document.title = "Rentopia"
-    }, [])
-*/
-  return (
-      <BrowserRouter sx = {{    width : "100%", Height : "100%"}}>
-          <Routes>
-              <Route path="/" >
-                  <Route index element={<HomeSite />} />
-                  <Route path="login" element={<LoginSite />} />
-                  <Route path="resetPassword" element={<ForgetPasswordSite/>}/>
-                  <Route path="register" element={<RegisterSite/>}/>
-              </Route>
-          </Routes>
-      </BrowserRouter>
-  );
+    /*    useEffect(() => {
+            document.title = "Rentopia"
+        }, [])
+    */
+    return (
+        <BrowserRouter sx={{ width: "100%", Height: "100%" }}>
+            <Routes>
+                <Route path="/" >
+                    <Route index element={<HomeSite />} />
+                    <Route path="login" element={<LoginSite />} />
+                    <Route path="resetPassword" element={<ForgetPasswordSite />} />
+                    <Route path="register" element={<RegisterSite />} />
+                </Route>
+                <Route path="device" >
+                    <Route index element={<Navigate to="/" />} /> // return to the Home page
+                    <Route path=":deviceId" element={<DevicePage />} />
+                </Route>
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
 export default App;
