05b692fef4a4372e2595d774ae6fb7dfb4f5b720
Jakob Pistohl <>
2024-12-06 13:14:41 +0100

    Fix DebugConttroller Device creation

--- src/main/java/com/othr/rentopia/controller/DebugController.java
+++ src/main/java/com/othr/rentopia/controller/DebugController.java
@@ -46,55 +46,87 @@ public class DebugController {
         // get id
         account = accountService.getAccount("test@xample.com");
 
-        Device device = new Device();
-        device.setTitle("Maschiene");
-        device.setDescription("Just better. trust me\ntext text text text text text text text text\ntext text text\ntext text text text text text text text text text text text text text text text text text text text");
-        device.setPrice(0.0);
-        device.setOwnerId(account.getId());
-        device.setLocation(location);
-        device.setIsPublic(true);
-        deviceService.saveDevice(device);
+        Device d0 = new Device();
+        d0.setTitle("Maschiene");
+        d0.setDescription("Just better. trust me\ntext text text text text text text text text\ntext text text\ntext text text text text text text text text text text text text text text text text text text text");
+        d0.setPrice(0.0);
+        d0.setOwnerId(account.getId());
+        d0.setLocation(location);
+        d0.setIsPublic(true);
+        deviceService.saveDevice(d0);
 
-        device.setTitle("Laptop");
-        device.setDescription("A powerful gaming laptop");
-        device.setPrice(1200.00);
-        deviceService.saveDevice(device);
+        Device d1 = new Device();
+        d1.setOwnerId(account.getId());
+        d1.setLocation(location);
+        d1.setIsPublic(false);
+        d1.setTitle("Laptop");
+        d1.setDescription("A powerful gaming laptop");
+        d1.setPrice(1200.00);
+        deviceService.saveDevice(d1);
 
-        device.setTitle("Portable Speaker");
-        device.setDescription("Take your music anywhere with this powerful, high-quality portable speaker.\nWith crystal-clear sound, deep bass, and a rugged, waterproof design, it's perfect for outdoor adventures.\nEnjoy up to 10 hours of continuous playback on a single charge.");
-        device.setPrice(15.00);
-        deviceService.saveDevice(device);
+        Device d2 = new Device();
+        d2.setOwnerId(account.getId());
+        d2.setLocation(location);
+        d2.setIsPublic(true);
+        d2.setTitle("Portable Speaker");
+        d2.setDescription("Take your music anywhere with this powerful, high-quality portable speaker.\nWith crystal-clear sound, deep bass, and a rugged, waterproof design, it's perfect for outdoor adventures.\nEnjoy up to 10 hours of continuous playback on a single charge.");
+        d2.setPrice(15.00);
+        deviceService.saveDevice(d2);
 
-        device.setTitle("3D Printer");
-        device.setDescription("Bring your ideas to life with this professional-grade 3D printer.\nCapable of printing high-quality models in various materials, it’s perfect for prototyping, creating custom designs, or even producing small items for sale.\nRent it for short-term projects or experimentation.");
-        device.setPrice(50.00);
-        deviceService.saveDevice(device);
+        Device d3 = new Device();
+        d3.setOwnerId(account.getId());
+        d3.setLocation(location);
+        d3.setIsPublic(true);
+        d3.setTitle("3D Printer");
+        d3.setDescription("Bring your ideas to life with this professional-grade 3D printer.\nCapable of printing high-quality models in various materials, it’s perfect for prototyping, creating custom designs, or even producing small items for sale.\nRent it for short-term projects or experimentation.");
+        d3.setPrice(50.00);
+        deviceService.saveDevice(d3);
 
-        device.setTitle("Projector");
-        device.setDescription("Turn any space into a home theater with this portable, high-definition projector.\nPerfect for movie nights, presentations, or projecting large images for events.\nCompatible with most devices and comes with built-in speakers.");
-        device.setPrice(25.00);
-        device.setIsPublic(true);
-        deviceService.saveDevice(device);
+        Device d4 = new Device();
+        d4.setOwnerId(account.getId());
+        d4.setLocation(location);
+        d4.setIsPublic(true);
+        d4.setTitle("Projector");
+        d4.setDescription("Turn any space into a home theater with this portable, high-definition projector.\nPerfect for movie nights, presentations, or projecting large images for events.\nCompatible with most devices and comes with built-in speakers.");
+        d4.setPrice(25.00);
+        d4.setIsPublic(true);
+        deviceService.saveDevice(d4);
 
-        device.setTitle("Drones with Camera");
-        device.setDescription("Capture stunning aerial footage with this drone equipped with a 4K camera.\nPerfect for outdoor photography, videography, or just exploring from above.\nGreat for travelers, photographers, and hobbyists who want to try drone flying without buying one.");
-        device.setPrice(40.00);
-        deviceService.saveDevice(device);
+        Device d5 = new Device();
+        d5.setOwnerId(account.getId());
+        d5.setLocation(location);
+        d5.setIsPublic(true);
+        d5.setTitle("Drones with Camera");
+        d5.setDescription("Capture stunning aerial footage with this drone equipped with a 4K camera.\nPerfect for outdoor photography, videography, or just exploring from above.\nGreat for travelers, photographers, and hobbyists who want to try drone flying without buying one.");
+        d5.setPrice(40.00);
+        deviceService.saveDevice(d5);
 
-        device.setTitle("GoPro Camera");
-        device.setDescription("Document your adventures with this rugged, waterproof GoPro camera.\nPerfect for sports enthusiasts, travelers, and vloggers who need a high-quality action camera that can withstand tough conditions.\nCapture 4K video and share your experiences with ease.");
-        device.setPrice(20.00);
-        deviceService.saveDevice(device);
+        Device d6 = new Device();
+        d6.setOwnerId(account.getId());
+        d6.setLocation(location);
+        d6.setIsPublic(true);
+        d6.setTitle("GoPro Camera");
+        d6.setDescription("Document your adventures with this rugged, waterproof GoPro camera.\nPerfect for sports enthusiasts, travelers, and vloggers who need a high-quality action camera that can withstand tough conditions.\nCapture 4K video and share your experiences with ease.");
+        d6.setPrice(20.00);
+        deviceService.saveDevice(d6);
 
-        device.setTitle("Electric Guitar");
-        device.setDescription("Learn or practice guitar with this high-quality electric guitar.\nComes with an amplifier and all the necessary accessories.\nGreat for beginners who want to try playing or musicians who need a temporary instrument for a gig.");
-        device.setPrice(30.00);
-        deviceService.saveDevice(device);
+        Device d7 = new Device();
+        d7.setOwnerId(account.getId());
+        d7.setLocation(location);
+        d7.setIsPublic(true);
+        d7.setTitle("Electric Guitar");
+        d7.setDescription("Learn or practice guitar with this high-quality electric guitar.\nComes with an amplifier and all the necessary accessories.\nGreat for beginners who want to try playing or musicians who need a temporary instrument for a gig.");
+        d7.setPrice(30.00);
+        deviceService.saveDevice(d7);
 
-        device.setTitle("Laser Cutter");
-        device.setDescription("Cut and engrave materials like wood, acrylic, and metal with this industrial-grade laser cutter.\nPerfect for makers, designers, or small business owners who need precision cutting for their projects.\nRent it for a few days to complete your next big idea.");
-        device.setPrice(75.00);
-        deviceService.saveDevice(device);
+        Device d8 = new Device();
+        d8.setOwnerId(account.getId());
+        d8.setLocation(location);
+        d8.setIsPublic(true);
+        d8.setTitle("Laser Cutter");
+        d8.setDescription("Cut and engrave materials like wood, acrylic, and metal with this industrial-grade laser cutter.\nPerfect for makers, designers, or small business owners who need precision cutting for their projects.\nRent it for a few days to complete your next big idea.");
+        d8.setPrice(75.00);
+        deviceService.saveDevice(d8);
 
     }
 }
