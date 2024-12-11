8fb3e5d0dd511c13b7bf6f7911dea959b48bfbb4
Jakob Pistohl <gh@jp-bits.de>
2024-12-11 11:10:33 +0100

    Allow multiple images

--- Frontend/src/sites/Device.js
+++ Frontend/src/sites/Device.js
@@ -70,6 +70,10 @@ function DeviceSite() {
 	navigate('/');
     }
 
+    const handleThumbnailClick = (index) => {
+	setDeviceImageIndex(index);
+    };
+
     return (
 	<Box>
 	    <Appbar authUser={authUser}/>
@@ -79,12 +83,46 @@ function DeviceSite() {
 		<Grid item xs={12} md={6}>
 		  <Box sx={{ textAlign: "center" }}>
 		    {/* Main Device Image */}
-		    <img src={"/images/devices/" + device.images[deviceImageIndex] } alt={device.title} width="100%" style={{ borderRadius: 8 }} />
+		    <img src={"/images/devices/" + device.images[deviceImageIndex % 2 /* TODO */] } alt={device.title} width="100%" style={{ borderRadius: 8 }} />
 		  </Box>
 
-		  {/* Image Thumbnails */}
-		  <ImageList sx={{ display: 'flex', justifyContent: 'center', mt: 2 }} cols={5}>
-
+		 {/* Image Thumbnails */}
+	         <ImageList
+		    sx={{
+		      display: 'flex',
+		      justifyContent: 'flex-start',
+		      overflowX: 'auto',
+		      mt: 2,
+		      pb: 1,
+		    }}
+		    cols={device.images.length}
+		    rowHeight={80}
+		  >
+		    {[...device.images, ...device.images, ...device.images, ...device.images, ...device.images, ...device.images].map((image, index) => (  /* TODO */
+		    // {device.images.map((image, index) => (
+		      <ImageListItem
+			key={index}
+			sx={{
+			  cursor: 'pointer',
+			  minWidth: 80,
+			  maxWidth: 100,
+			  flexShrink: 0,
+			}}
+		      >
+			<img
+			  src={"/images/devices/" + image}
+			  alt={`${device.title} thumbnail ${index + 1}`}
+			  style={{
+			    borderRadius: 8,
+			    border: index === deviceImageIndex ? '2px solid #1976d2' : '2px solid transparent',
+			    width: '100%',
+			    height: '100%',
+			    objectFit: 'cover',
+			  }}
+			  onClick={() => handleThumbnailClick(index)}
+			/>
+		      </ImageListItem>
+		    ))}
 		  </ImageList>
 		</Grid>
 
@@ -124,7 +162,7 @@ function DeviceSite() {
 		    <Box display="flex" alignItems="center" mb={2}>
 		      <Avatar alt={"seller_name"} src={""} sx={{ mr: 1 }} />
 		      <Typography variant="body2" color="textSecondary">
-			Seller: {device.owner}
+			{device.owner}
 		      </Typography>
 		    </Box>
 
