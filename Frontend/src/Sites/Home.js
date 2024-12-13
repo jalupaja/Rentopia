e513e1c4ba2366f29d3608e2c8c441f9b1593b50
Andreas <awuehrl@gmail.com>
2024-12-13 01:23:21 +0100

    added Pagination to Home Page

--- Frontend/src/Sites/Home.js
+++ Frontend/src/Sites/Home.js
@@ -3,13 +3,16 @@ import {
     Card,
     CardActionArea, CardContent,
     CardMedia,
-    styled, Typography, Grid2
+    styled, Typography, Grid2, Pagination
 } from "@mui/material";
 import { useNavigate } from "react-router-dom";
 import Footer from "../components/Footer.js";
 import FetchBackend, {JWTTokenExists} from "../helper/BackendHelper.js";
 import {useEffect, useState} from "react";
+import * as React from 'react';
 import Appbar from "../components/Appbar.js";
+import usePagination from "../components/Pagination";
+import {FrameStyle} from "./RegisterPage";
 
 const DeviceGrid = styled(Grid2)(({theme}) => ({
     margin: '2% 10%'
@@ -17,8 +20,13 @@ const DeviceGrid = styled(Grid2)(({theme}) => ({
 
 function Home(){
     const navigate = useNavigate();
-
     const [authUser, setAuthUser] = useState(null);
+    const [page, setPage] = useState(1);
+    const [devices, setDevices] = useState([1,2,3,4,5,6,7,8,9,10,11,12,13,14]);
+    const devicesPPage = 10;
+
+    const count = Math.ceil(devices.length / devicesPPage);
+    const _DEVICES = usePagination(devices, devicesPPage);
 
     useEffect(() =>{
         if(JWTTokenExists()){
@@ -32,26 +40,31 @@ function Home(){
 
     }, []);
 
+    const handlePageChange = (event, value) => {
+        setPage(value);
+        _DEVICES.jump(value);
+    };
+
     return (
-        <Box>
+        <Box sx={{...FrameStyle}}>
             <Appbar authUser={authUser} searchVisibility={'visible'}/>
             <Box sx={{ flexGrow: 1}} >
                 <DeviceGrid container spacing={{xs: 4}} justifyContent={'center'} >
-                    {Array.from({ length: 15 }).map((_, index) => (
+                    {_DEVICES.currentData().map((item, index) => (
                         <Grid2 key={index}>
                             <Card sx={{width: 300, boxShadow: 3}} >
                                 <CardActionArea /*component={RouterLink} to="/DevicesDetail" TODO: ADD Device Detail PAge here*/>
                                     <CardMedia
                                         component="img"
-                                        alt={"ToolNr " + index}
+                                        alt={"ToolNr " + item}
                                         height="125"
                                         image="" />
                                     <CardContent>
                                         <Typography gutterBottom variant="h6" component="div">
-                                            {"ToolNr " + index}
+                                            {"ToolNr " + item}
                                         </Typography>
                                         <Typography variant="h6" sx={{ color: 'text.secondary' }} justifySelf={'end'}>
-                                            {index * 3 + " €"}
+                                            {item * 3 + " €"}
                                         </Typography>
                                     </CardContent>
                                 </CardActionArea>
@@ -60,6 +73,8 @@ function Home(){
                     ))}
                 </DeviceGrid>
             </Box>
+            <Box flex={"auto"}/>
+            <Pagination count={count} page={page} onChange={handlePageChange} />
             <Footer/>
         </Box>
     );
