d1f02832ea21a7cf1f016e293a06a8a5117a2abf
Florian-Preissner <flo.preissner@gmail.com>
2024-12-14 20:03:40 +0100

    Add prototype for ticket detail site

--- Frontend/src/sites/HelpCenterSite.js
+++ Frontend/src/sites/HelpCenterSite.js
@@ -1,23 +1,58 @@
 import Appbar from "../components/Appbar.js";
 import * as React from "react";
-import {Box, Typography} from "@mui/material";
+import {Box, Typography, Grid2, Button, ListItem, List, ListItemButton, ListItemText} from "@mui/material";
 import Footer from "../components/Footer.js";
-import FetchBackend, {GetAuthUser, JWTTokenExists} from "../helper/BackendHelper.js";
-import {useEffect, useState} from "react";
+import FetchBackend, {
+    GetAuthUser,
+    ReturnHomeWhenLoggedOut
+} from "../helper/BackendHelper.js";
+import TicketDetail from "../components/TicketDetailComponent.js";
 
 function HelpCenterSite(){
+    ReturnHomeWhenLoggedOut();
+
     let authUser = GetAuthUser();
 
-    let username = null;
-    if(authUser !== null){
-        username = authUser.name;
+    //const [selectedIndex, setSelectedIndex] = React.useState(-1);
+    const [tickets, setTickets] = React.useState([]);
+    const [ticketDetailComponent, setTicketDetailComponent] = React.useState(null);
+
+    const handleTicketSelection = (index) => {
+        if(tickets != null && index >= 0 && index < tickets.length){
+            setTicketDetailComponent(<TicketDetail ticket={tickets[index]}/>);
+        }
+    }
+    const addNewTicket = () =>{
+        setTickets([
+            ...tickets,
+            {
+                id : "-1",
+                title : "new ticket",
+                status : "new"
+            }
+        ]);
+
+        //todo
+        //handleTicketSelection(tickets.length-1);
     }
     return (<Box>
         <Appbar authUser={authUser}/>
-            <Box sx={{flexGrow: 1}}/>
-            <Typography variant="button" gutterBottom variant="h6">
-                Hello {username}
-            </Typography>
+            <Grid2 container>
+                <Grid2 size={3} sx={{padding : "1%"}}>
+                    <Button variant="contained" onClick={addNewTicket}>New Ticket</Button>
+                    <List>
+                        {tickets.map((ticket, index) => (
+                            <ListItemButton
+                                key={index} onClick={() => handleTicketSelection(index)}>{ticket.title}
+                            </ListItemButton>
+                            )
+                        )}
+                    </List>
+
+                </Grid2>
+                <Grid2 size={9}>{ticketDetailComponent}</Grid2>
+            </Grid2>
+
         <Footer/>
     </Box>
     );
